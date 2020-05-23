1c17b45c9bfb56c2c81148ba64eac2efe7468c27
abatui <abatuiev@homeaway.com>
2020-05-23 12:57:37 +0300

    add jsonproperty access

--- src/main/java/com/course/project/businessmanager/dto/BuildingDTO.java
+++ src/main/java/com/course/project/businessmanager/dto/BuildingDTO.java
@@ -1,13 +1,12 @@
 package com.course.project.businessmanager.dto;
 
 import com.course.project.businessmanager.entity.Business;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
-@JsonIgnoreProperties(value = { "business" })
 public class BuildingDTO {
     private UUID id;
 
@@ -15,5 +14,6 @@ public class BuildingDTO {
 
     private String geolocation;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Business business;
 }
