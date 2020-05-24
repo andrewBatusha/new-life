0b3d44c1e0a4fe3fbfb2cf9120b27c31933371e7
abatui <abatuiev@homeaway.com>
2020-05-24 20:42:25 +0300

    add valid

--- src/main/java/com/course/project/businessmanager/entity/Note.java
+++ src/main/java/com/course/project/businessmanager/entity/Note.java
@@ -47,7 +47,7 @@ public class Note implements Serializable {
     @Enumerated(EnumType.STRING)
     private NoteStatus noteStatus;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
