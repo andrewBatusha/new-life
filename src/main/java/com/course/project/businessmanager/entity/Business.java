c6ec54892702067fd00623dafbbf808d13738d1a
abatui <abatuiev@homeaway.com>
2020-05-24 14:20:04 +0300

    merge cascade

--- src/main/java/com/course/project/businessmanager/entity/Business.java
+++ src/main/java/com/course/project/businessmanager/entity/Business.java
@@ -60,7 +60,7 @@ public class Business implements Serializable {
     )
     private List<User> users = new ArrayList<>();
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "business")
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "business", cascade = CascadeType.MERGE)
     private List<Building> buildings = new ArrayList<>();
 
 }
