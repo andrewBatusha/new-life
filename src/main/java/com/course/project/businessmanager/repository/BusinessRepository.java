47ab24329417fc6d46d2fda414bee9555fb2aa7c
abatui <abatuiev@homeaway.com>
2020-05-28 01:14:16 +0300

    fix Business

--- src/main/java/com/course/project/businessmanager/repository/BusinessRepository.java
+++ src/main/java/com/course/project/businessmanager/repository/BusinessRepository.java
@@ -1,9 +1,11 @@
 package com.course.project.businessmanager.repository;
 
 import com.course.project.businessmanager.entity.Business;
+import com.course.project.businessmanager.entity.User;
 
 import java.util.UUID;
 
 public interface BusinessRepository extends BasicRepository<Business, UUID> {
     Long countBusinessWithId(UUID uuid);
+    Business update(Business entity);
 }
--- src/main/java/com/course/project/businessmanager/repository/impl/BusinessRepositoryImpl.java
+++ src/main/java/com/course/project/businessmanager/repository/impl/BusinessRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.course.project.businessmanager.repository.impl;
 
 import com.course.project.businessmanager.entity.Business;
+import com.course.project.businessmanager.entity.User;
 import com.course.project.businessmanager.repository.BusinessRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
@@ -38,5 +39,19 @@ public class BusinessRepositoryImpl extends BasicRepositoryImpl<Business, UUID> 
         return (Long) getSession().createQuery
                 ("SELECT count (*) FROM Business b WHERE b.id = :uuid")
                 .setParameter("uuid", uuid).getSingleResult();
+    }
+
+    /**
+     * Modified update method, which merge entity before updating it
+     *
+     * @param entity user is going to be updated
+     * @return Business
+     */
+    @Override
+    public Business update(Business entity) {
+        log.info("Enter into update method with entity:{}", entity);
+        entity = (Business) getSession().merge(entity);
+        getSession().update(entity);
+        return entity;
     }
 }
